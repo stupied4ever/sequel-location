e87af9e4afc68ca891ef4b4d234975b3a00f7a4e
Mitchell Henke <mitchh23@gmail.com>
2014-09-25 13:13:20 -0500

    Pull out location search into own method

--- lib/sequel/plugins/pg_location.rb
+++ lib/sequel/plugins/pg_location.rb
@@ -22,12 +22,20 @@ module Sequel
       module DatasetMethods
         def nearest(lat,lng,radius)
           location_cache_field = model.location_cache_field
+
+          within(lat, lng, radius)
+          .select_append{
+            (Sequel::SQL::AliasedExpression.new(Sequel.function(:earth_distance, Sequel.function(:ll_to_earth,lat,lng), location_cache_field), :distance))
+          }.order(:distance.asc)
+        end
+
+        def within(lat,lng,radius)
+          location_cache_field = model.location_cache_field
           radius_in_km = radius.to_f * 1609.3
           lat = lat.to_f
           lng = lng.to_f
-          where("earth_box(ll_to_earth(?,?),?) @> #{location_cache_field}", lat, lng, radius_in_km).where("earth_distance(ll_to_earth(?, ?), #{location_cache_field}) < ?", lat, lng, radius_in_km).select_append{
-            (Sequel::SQL::AliasedExpression.new(Sequel.function(:earth_distance, Sequel.function(:ll_to_earth,lat,lng), location_cache_field), :distance))
-          }.order(:distance.asc)
+
+          where("earth_box(ll_to_earth(?,?),?) @> #{location_cache_field}", lat, lng, radius_in_km).where("earth_distance(ll_to_earth(?, ?), #{location_cache_field}) < ?", lat, lng, radius_in_km)
         end
       end
     end

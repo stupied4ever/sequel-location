355182772b397f0c3ccd6ee58a48658ac190fc62
Amadeus Folego <amadeusfolego@gmail.com>
2015-10-21 19:22:19 -0200

    Add #near_to and rename #nearest -> #near_within

--- lib/sequel/plugins/pg_location.rb
+++ lib/sequel/plugins/pg_location.rb
@@ -16,26 +16,29 @@ module Sequel
           subclass.instance_variable_set(:@location_cache_field, instance_variable_get(:@location_cache_field))
         end
 
-        Plugins.def_dataset_methods(self, :nearest)
+        Plugins.def_dataset_methods(self, :near_to)
       end
 
       module DatasetMethods
-        def nearest(lat,lng,radius)
+        def near_to(lat, lng)
           location_cache_field = model.location_cache_field
 
-          within(lat, lng, radius)
-          .select_append{
-            (Sequel::SQL::AliasedExpression.new(Sequel.function(:earth_distance, Sequel.function(:ll_to_earth,lat,lng), location_cache_field), :distance))
+          select_append{ Sequel::SQL::AliasedExpression.new(
+            Sequel.function(:earth_distance,
+              Sequel.function(:ll_to_earth, lat, lng),
+                location_cache_field), :distance)
           }.order(Sequel.asc(Sequel.lit("distance")))
         end
 
-        def within(lat,lng,radius)
-          location_cache_field = model.location_cache_field
-          radius_in_meters = radius.to_f
-          lat = lat.to_f
-          lng = lng.to_f
+        def near_within(lat, lng, radius)
+          within(lat, lng, radius).near_to(lat, lng)
+        end
 
-          where("earth_box(ll_to_earth(?,?),?) @> #{location_cache_field}", lat, lng, radius_in_meters).where("earth_distance(ll_to_earth(?, ?), #{location_cache_field}) < ?", lat, lng, radius_in_meters)
+        def within(lat, lng, radius)
+          location_cache_field = model.location_cache_field
+
+          where("earth_box(ll_to_earth(?,?),?) @> #{location_cache_field}", lat, lng, radius).
+            where("earth_distance(ll_to_earth(?, ?), #{location_cache_field}) < ?", lat, lng, radius)
         end
       end
     end
